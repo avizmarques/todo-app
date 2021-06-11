5d00a3c5e46a2c8b51f8fb733166cfc9232be9b5
avizmarques <aviz.marques@gmail.com>
2021-06-11 12:58:18 +0200

    added filters

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,9 +1,35 @@
-export default function FilterBar() {
+import cn from "classnames";
+
+export default function FilterBar({ filter, setFilter }) {
   return (
     <div className="flex text-sm">
-      <div className="mr-2 hover:font-black hover:text-gray-800">All</div>
-      <div className="mr-2 hover:font-black hover:text-gray-800">Active</div>
-      <div className="hover:font-black hover:text-gray-800">Completed</div>
+      <button
+        className={cn(
+          "mr-2 hover:font-black hover:text-gray-800 focus:outline-none",
+          filter === "all" && "font-black text-gray-800"
+        )}
+        onClick={() => setFilter("all")}
+      >
+        All
+      </button>
+      <button
+        className={cn(
+          "mr-2 hover:font-black hover:text-gray-800 focus:outline-none",
+          filter === "active" && "font-black text-gray-800"
+        )}
+        onClick={() => setFilter("active")}
+      >
+        Active
+      </button>
+      <button
+        className={cn(
+          "hover:font-black hover:text-gray-800 focus:outline-none",
+          filter === "completed" && "font-black text-gray-800"
+        )}
+        onClick={() => setFilter("completed")}
+      >
+        Completed
+      </button>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,13 +8,15 @@ import FilterBar from "./FilterBar";
 export default function TodoList() {
   const { list, setList } = useContext(ListContext);
 
+  const activeList = list.filter((todo) => !todo.checked);
+  const completedList = list.filter((todo) => todo.checked);
+
+  const [filter, setFilter] = useState("all");
+  console.log(filter);
+
   const [draggedIndex, setDraggedIndex] = useState(null);
   const [draggedOverIndex, setDraggedOverIndex] = useState(null);
   const [newList, setNewList] = useState(list);
-
-  const unchecked = list.filter((todo) => {
-    return !todo.checked;
-  });
 
   const onDragStart = (e, i) => {
     e.dataTransfer.effectAllowed = "move";
@@ -45,32 +47,61 @@ export default function TodoList() {
   };
 
   const clearCompleted = () => {
-    const clearedList = list.filter((todo) => !todo.checked);
-    setList(clearedList);
+    setList(activeList);
   };
 
   return (
     <div className="w-1/3 rounded-md overflow-hidden">
       <ul>
-        {list.map((todo, i) => (
-          <li
-            draggable
-            onDragStart={(e) => onDragStart(e, i)}
-            onDragOver={() => onDragOver(i)}
-            onDragEnd={() => onDragEnd()}
-            className={cn(
-              "border-b-2",
-              draggedOverIndex === i && "border-grayishBlue-dark"
-            )}
-          >
-            <TodoItem todo={todo} />
-          </li>
-        ))}
+        {filter === "active"
+          ? activeList.map((todo, i) => (
+              <li
+                draggable
+                onDragStart={(e) => onDragStart(e, i)}
+                onDragOver={() => onDragOver(i)}
+                onDragEnd={() => onDragEnd()}
+                className={cn(
+                  "border-b-2",
+                  draggedOverIndex === i && "border-grayishBlue-dark"
+                )}
+              >
+                <TodoItem todo={todo} />
+              </li>
+            ))
+          : filter === "completed"
+          ? completedList.map((todo, i) => (
+              <li
+                draggable
+                onDragStart={(e) => onDragStart(e, i)}
+                onDragOver={() => onDragOver(i)}
+                onDragEnd={() => onDragEnd()}
+                className={cn(
+                  "border-b-2",
+                  draggedOverIndex === i && "border-grayishBlue-dark"
+                )}
+              >
+                <TodoItem todo={todo} />
+              </li>
+            ))
+          : list.map((todo, i) => (
+              <li
+                draggable
+                onDragStart={(e) => onDragStart(e, i)}
+                onDragOver={() => onDragOver(i)}
+                onDragEnd={() => onDragEnd()}
+                className={cn(
+                  "border-b-2",
+                  draggedOverIndex === i && "border-grayishBlue-dark"
+                )}
+              >
+                <TodoItem todo={todo} />
+              </li>
+            ))}
       </ul>
       {list.length > 0 && (
         <div className="bg-white h-16 flex items-center justify-between px-6 text-gray-400">
-          <div className="text-sm">{unchecked.length} item(s) left</div>
-          <FilterBar />
+          <div className="text-sm">{activeList.length} item(s) left</div>
+          <FilterBar filter={filter} setFilter={setFilter} />
           <button
             className="text-sm hover:font-black hover:text-gray-800 focus:outline-none"
             onClick={clearCompleted}
